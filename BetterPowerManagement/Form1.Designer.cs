3277020a96edb3e71e845af911a01f5e35cb2b6e
Shawn Benoit <70031119+benoitsd@users.noreply.github.com>
2023-02-01 03:12:57 -0600

    Add project files.

---
+++ BetterPowerManagement/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BetterPowerManagement
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.Plan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.guidID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(7, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Status:";
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Plan,
+            this.guidID});
+			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(10, 32);
+			this.listView1.MultiSelect = false;
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(415, 268);
+			this.listView1.TabIndex = 2;
+			this.listView1.TileSize = new System.Drawing.Size(50, 50);
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			// 
+			// Plan
+			// 
+			this.Plan.Text = "Plan";
+			this.Plan.Width = 160;
+			// 
+			// guidID
+			// 
+			this.guidID.Text = "guid";
+			this.guidID.Width = 240;
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 342);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "label3";
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.BackColor = System.Drawing.Color.Firebrick;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(10, 305);
+			this.button1.Margin = new System.Windows.Forms.Padding(2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(419, 33);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Exit";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(437, 360);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Power Toggler";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ColumnHeader Plan;
+		private System.Windows.Forms.ColumnHeader guidID;
+	}
+}
+
---
+++ BetterPowerManagement/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace BetterPowerManagement
+{
+
+
+	public partial class Form1 : Form
+	{
+
+		[DllImport("PowrProf.dll")]
+		public static extern UInt32 PowerEnumerate(IntPtr RootPowerKey, IntPtr SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, UInt32 AcessFlags, UInt32 Index, ref Guid Buffer, ref UInt32 BufferSize);
+
+		[DllImport("PowrProf.dll")]
+		public static extern UInt32 PowerReadFriendlyName(IntPtr RootPowerKey, ref Guid SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, IntPtr PowerSettingGuid, IntPtr Buffer, ref UInt32 BufferSize);
+
+		public enum AccessFlags : uint
+		{
+			ACCESS_SCHEME = 16,
+			ACCESS_SUBGROUP = 17,
+			ACCESS_INDIVIDUAL_SETTING = 18
+		}
+
+		private static string ReadFriendlyName(Guid schemeGuid)
+		{
+			uint sizeName = 1024;
+			IntPtr pSizeName = Marshal.AllocHGlobal((int)sizeName);
+
+			string friendlyName;
+
+			try
+			{
+				PowerReadFriendlyName(IntPtr.Zero, ref schemeGuid, IntPtr.Zero, IntPtr.Zero, pSizeName, ref sizeName);
+				friendlyName = Marshal.PtrToStringUni(pSizeName);
+			}
+			finally
+			{
+				Marshal.FreeHGlobal(pSizeName);
+			}
+
+			return friendlyName;
+		}
+
+		public static IEnumerable<Guid> GetAll()
+		{
+			var schemeGuid = Guid.Empty;
+
+			uint sizeSchemeGuid = (uint)Marshal.SizeOf(typeof(Guid));
+			uint schemeIndex = 0;
+
+			while(PowerEnumerate(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, (uint)AccessFlags.ACCESS_SCHEME, schemeIndex, ref schemeGuid, ref sizeSchemeGuid) == 0)
+			{
+				yield return schemeGuid;
+				schemeIndex++;
+			}
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+			label1.Text = "%" + ((SystemInformation.PowerStatus.BatteryLifePercent) * 100).ToString();
+			listSchemes();
+
+		}
+
+		public void listSchemes()
+		{
+
+			listView1.View = View.Details;
+			listView1.FullRowSelect = true;
+			listView1.Columns.Add("Plan", 160, HorizontalAlignment.Left);
+			listView1.Columns.Add("GUID", 240, HorizontalAlignment.Left);
+
+
+
+			var guidPlans = GetAll();
+
+			foreach(Guid guidPlan in guidPlans)
+			{
+				listView1.Items.Add(ReadFriendlyName(guidPlan), "Plan");
+				listView1.Items.Add(guidPlan.ToString(), "GUID");
+			}
+
+		}
+
+		private void ListView1_ItemActivate(Object sender, EventArgs e)
+		{
+
+			MessageBox.Show("You are in the ListView.ItemActivate event.");
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string selectedItemName = listView1.SelectedItems.ToString();
+
+			//System.Diagnostics.Process process = new System.Diagnostics.Process();
+			//System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+			//startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+			//startInfo.FileName = "cmd.exe";
+			//startInfo.Arguments = "/c powercfg /setactive " + guid;
+			//process.StartInfo = startInfo;
+			//process.Start();
+
+			label3.Text = selectedItemName.Substring(0);// ToString();
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
