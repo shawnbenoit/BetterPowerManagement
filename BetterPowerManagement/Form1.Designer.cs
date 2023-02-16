9288ef08e8f19c72e050660e5f9e65b97599f1f9
Shawn Benoit <benoitsd@gmail.com>
2023-02-16 06:01:20 -0600

    u2

--- BetterPowerManagement/Form1.Designer.cs
+++ BetterPowerManagement/Form1.Designer.cs
@@ -93,7 +93,7 @@
 			this.button1.Location = new System.Drawing.Point(10, 290);
 			this.button1.Margin = new System.Windows.Forms.Padding(2);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(970, 33);
+			this.button1.Size = new System.Drawing.Size(944, 33);
 			this.button1.TabIndex = 4;
 			this.button1.Text = "Exit";
 			this.button1.UseVisualStyleBackColor = false;
@@ -114,7 +114,7 @@
 			this.button2.ForeColor = System.Drawing.Color.White;
 			this.button2.Location = new System.Drawing.Point(10, 255);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(970, 30);
+			this.button2.Size = new System.Drawing.Size(944, 30);
 			this.button2.TabIndex = 5;
 			this.button2.Text = "Import Ultra Power Saver";
 			this.button2.UseVisualStyleBackColor = false;
@@ -130,7 +130,7 @@
 			this.button3.ForeColor = System.Drawing.Color.White;
 			this.button3.Location = new System.Drawing.Point(10, 219);
 			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(970, 30);
+			this.button3.Size = new System.Drawing.Size(944, 30);
 			this.button3.TabIndex = 6;
 			this.button3.Text = "Import Ultra Performance";
 			this.button3.UseVisualStyleBackColor = false;
@@ -151,7 +151,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.BackColor = System.Drawing.Color.Black;
-			this.ClientSize = new System.Drawing.Size(992, 395);
+			this.ClientSize = new System.Drawing.Size(966, 395);
 			this.ControlBox = false;
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.button3);
